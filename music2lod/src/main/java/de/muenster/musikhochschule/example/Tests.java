d8f835d14c538efb7d5a6fe89fe5fb0a2d9734f6
Jim Jones <jimjonesbr@gmail.com>
2016-06-16 13:16:54 +0200

    Sample queries added.

--- music2lod/src/main/java/de/muenster/musikhochschule/example/Tests.java
+++ music2lod/src/main/java/de/muenster/musikhochschule/example/Tests.java
@@ -13,24 +13,25 @@ public class Tests {
 		parser.setExportFolder("nt/");
 		parser.setVerbose(false);
 
-		parser.loadMusicXML(new File("scores/xmlsamples/Elgar_cello_Concerto_Op.85.xml"));
-		parser.printScoreDetails();
 		
-		parser.exportAsNTriples();
+//		parser.loadMusicXML(new File("scores/xmlsamples/Elgar_cello_Concerto_Op.85.xml"));
+//		parser.printScoreDetails();
+//		
+//		parser.exportAsNTriples();
 
 		
-//		File[] files = new File("scores/sammlung/").listFiles();
-//		
-//		for (File file : files) {
-//
-//			if(file.getName().endsWith(".xml")){
-//
-//				parser.loadMusicXML(file);
-//				parser.exportAsNTriples();
-//				
-//			}
-//			
-//		}
+		File[] files = new File("scores/sammlung/").listFiles();
+		
+		for (File file : files) {
+
+			if(file.getName().endsWith(".xml")){
+
+				parser.loadMusicXML(file);
+				parser.exportAsNTriples();
+				
+			}
+			
+		}
 	
 	}
 
